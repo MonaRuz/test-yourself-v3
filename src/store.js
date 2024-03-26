0cafbe01ffcefeb11c30eebcc0ad60b4bee6dde9
MonaRuz <mona.ruz@seznam.cz>
2024-03-26 21:18:45 +0100

    applied thunk in store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
-import { combineReducers, createStore } from "redux"
+import { applyMiddleware, combineReducers, createStore } from "redux"
 import categoriesReducer from "./features/categories/categoriesSlice"
 import categoryTestReducer from "./features/categoryTest/categoryTestSlice"
 import fakeAuthReducer from "./features/fakeAuth/fakeAuthSlice"
 import questionsReducer from "./features/questions/questionsSlice"
+import { thunk } from "redux-thunk"
 
 const rootReducer = combineReducers({
 	categories: categoriesReducer,
@@ -11,5 +12,5 @@ const rootReducer = combineReducers({
 	authenticate: fakeAuthReducer,
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(thunk))
  export default store
