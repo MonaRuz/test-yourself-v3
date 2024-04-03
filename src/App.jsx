2c6a1aea4b177efeb35301dcf5afafd40ab1658b
MonaRuz <mona.ruz@seznam.cz>
2024-04-03 20:40:01 +0200

    Rewrited router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {
+	BrowserRouter,
+	Route,
+	RouterProvider,
+	Routes,
+	createBrowserRouter,
+} from "react-router-dom"
 import { QuestionsProvider } from "./contexts/QuestionsContext"
 import Test from "./features/categoryTest/Test"
 import PageNotFound from "./UI/PageNotFound"
@@ -10,74 +16,130 @@ import NewQuestion from "./features/questions/NewQuestion"
 import ProtectedRoute from "./features/fakeAuth/ProtectedRoute"
 import { Suspense, lazy } from "react"
 import Loader from "./UI/Loader"
+import Error from "./UI/Error"
 
 const Homepage = lazy(() => import("./UI/Homepage"))
 const Login = lazy(() => import("./features/fakeAuth/Login"))
 const AppLayout = lazy(() => import("./UI/AppLayout"))
 
-export default function App() {
-	return (
-		<QuestionsProvider>
-			<BrowserRouter>
-				<Suspense fallback={<Loader />}>
-					<Routes>
-						<Route
-							index
-							element={<Homepage />}
-						/>
-						<Route
-							path='/'
-							element={<Homepage />}
-						/>
-						<Route
-							path='login'
-							element={<Login />}
-						/>
+const router = createBrowserRouter([
+	{
+		element: (
+			<Suspense fallback={<Loader />}>
+				<Routes />
+			</Suspense>
+		),
+		errorElement: <Error />,
+		children: [
+			{
+				path: "/",
+				element: <Homepage />,
+			},
+			{
+				path: "/login",
+				element: <Login />,
+			},
+			{
+				path: "/app",
+				element: (
+					<ProtectedRoute>
+						<AppLayout />
+					</ProtectedRoute>
+				),
+				children: [
+					{
+						path: "/categories",
+						element: <Categories />,
+					},
+					{
+						path: "/test/:category",
+						element: <Test />,
+					},
+					{ 
+						path: "/questions/:category", 
+						element: <Questions /> 
+					},
+					{
+						path:'/questions/:category/new-question',
+						element:<NewQuestion />
+					},
+					{
+						path:'/questions/:category/:id',
+						element:<Edit />
+					},
+					{
+						path:'/new-category',
+						element:<CreateNewCategory />
+					}
+				],
+			},
+		],
+	},
+])
 
-						<Route
-							path='app'
-							element={
-								<ProtectedRoute>
-									<AppLayout />
-								</ProtectedRoute>
-							}
-						>
-							<Route
-								index
-								element={<Categories />}
-							/>
-							<Route
-								path='categories'
-								element={<Categories />}
-							/>
-							<Route
-								path='test/:category'
-								element={<Test />}
-							/>
-							<Route
-								path='questions/:category'
-								element={<Questions />}
-							/>
-							<Route
-								path='questions/:category/new-question'
-								element={<NewQuestion />}
-							/>
-							<Route
-								path='questions/:category/:id'
-								element={<Edit />}
-							/>
-							<Route
-								path='new-category'
-								element={<CreateNewCategory />}
-							/>
-						</Route>
-						<Route
-							path='*'
-							element={<PageNotFound />}
-						/>
-					</Routes>
-				</Suspense>
-			</BrowserRouter>
-		</QuestionsProvider>
-	)
+export default function App() {
+	return <RouterProvider router={router}/>
+		// <QuestionsProvider>
+		// 	<BrowserRouter>
+		// 		<Suspense fallback={<Loader />}>
+		// 			<Routes>
+		// 				<Route
+		// 					index
+		// 					element={<Homepage />}
+		// 				/>
+		// 				<Route
+		// 					path='/'
+		// 					element={<Homepage />}
+		// 				/>
+		// 				<Route
+		// 					path='login'
+		// 					element={<Login />}
+		// 				/>
+
+		// 				<Route
+		// 					path='app'
+		// 					element={
+		// 						<ProtectedRoute>
+		// 							<AppLayout />
+		// 						</ProtectedRoute>
+		// 					}
+		// 				>
+		// 					<Route
+		// 						index
+		// 						element={<Categories />}
+		// 					/>
+		// 					<Route
+		// 						path='categories'
+		// 						element={<Categories />}
+		// 					/>
+		// 					<Route
+		// 						path='test/:category'
+		// 						element={<Test />}
+		// 					/>
+		// 					<Route
+		// 						path='questions/:category'
+		// 						element={<Questions />}
+		// 					/>
+		// 					<Route
+		// 						path='questions/:category/new-question'
+		// 						element={<NewQuestion />}
+		// 					/>
+		// 					<Route
+		// 						path='questions/:category/:id'
+		// 						element={<Edit />}
+		// 					/>
+		// 					<Route
+		// 						path='new-category'
+		// 						element={<CreateNewCategory />}
+		// 					/>
+		// 				</Route>
+		// 				<Route
+		// 					path='*'
+		// 					element={<PageNotFound />}
+		// 				/>
+		// 			</Routes>
+		// 		</Suspense>
+		// 	</BrowserRouter>
+		// </QuestionsProvider>
+	
 }
