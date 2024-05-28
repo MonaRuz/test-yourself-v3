49155da3f626985564220b4269079e21142aaaa4
MonaRuz <mona.ruz@seznam.cz>
2024-05-28 11:01:57 +0200

    finished styling for mobile

--- src/features/categoryTest/Results.jsx
+++ src/features/categoryTest/Results.jsx
@@ -4,23 +4,25 @@ import Button from "../../UI/Button"
 import styles from "./Results.module.css"
 
 export default function Results() {
-	const { result,getQuestions,dispatch } = useQuestions()
+	const { result, getQuestions, dispatch } = useQuestions()
 
-  const navigate=useNavigate()
-  const {category}=useParams()
-  function handleBack(){
-    navigate(-1)
-  }
-  function handleRestart(){
-    getQuestions(category)
-    dispatch({type:"restart"})
-  }
+	const navigate = useNavigate()
+	const { category } = useParams()
+	function handleBack() {
+		navigate(-1)
+	}
+	function handleRestart() {
+		getQuestions(category)
+		dispatch({ type: "restart" })
+	}
 
 	return (
 		<div>
-			<p className={styles.result}>Úspěšnost testu : {result}%</p>
-      <Button type="negative" onClick={handleBack}>Back</Button>
-      <Button type="confirm" onClick={handleRestart}>Restart</Button>
+			<p className="text-blue-200 my-5">Úspěšnost testu : {result}%</p>
+			<div className="flex justify-between">
+				<button className="text-yellow-200 border border-yellow-200 px-3 py-2" onClick={handleBack}>Back</button>
+				<button className="text-yellow-200 border border-yellow-200 px-3 py-2" onClick={handleRestart}>Restart</button>
+			</div>
 		</div>
 	)
 }
