2e84021e8ff679819ea3b03f608fa23f172f285c
MonaRuz <mona.ruz@seznam.cz>
2024-06-20 19:14:25 +0200

    Created controlled elements in createNewCategory

--- src/features/categories/CreateNewCategory.jsx
+++ src/features/categories/CreateNewCategory.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react"
+
 export default function CreateNewCategory() {
+
+  const[newCategoryName,setNewCategoryName]=useState("")
+  const[newCategoryQuestion,setNewCategoryQuestion]=useState("")
+  const[newCategoryAnswer,setNewCategoryAnswer]=useState("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    
+
+    setNewCategoryName("")
+    setNewCategoryQuestion("")
+    setNewCategoryAnswer("")
+  }
+
+  console.log(newCategoryName);
+    console.log(newCategoryQuestion);
+    console.log(newCategoryAnswer);
+
 	return (
 		<div className='text-blue-200 text-center'>
 			<p className="text-sm pt-2">
 				Input name of your new category and your first question and answer in
 				this category.
 			</p>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label>
 					<p className="text-orange-200 py-2">Category name:</p>
 					<input
+            value={newCategoryName}
+            onChange={(e)=>setNewCategoryName(e.target.value)}
 						className='bg-zinc-800 border border-yellow-200 w-full'
 						type='text'
 					></input>
@@ -16,6 +39,8 @@ export default function CreateNewCategory() {
 				<label>
 					<p className="text-orange-200 py-2">Your first question:</p>
 					<textarea
+          value={newCategoryQuestion}
+          onChange={(e)=>setNewCategoryQuestion(e.target.value)}
 						className='bg-zinc-800 border border-yellow-200 w-full'
 
 	
@@ -24,6 +49,8 @@ export default function CreateNewCategory() {
 				<label>
 					<p className="text-orange-200 py-2">Your first answer:</p>
 					<textarea
+          value={newCategoryAnswer}
+          onChange={(e)=>setNewCategoryAnswer(e.target.value)}
 						className='bg-zinc-800 border border-yellow-200 w-full'
 					></textarea>
 				</label>
